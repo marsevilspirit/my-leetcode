f9d7192f42680eb0f147cb3559f1f807e8fee019
marsevilspirit <a2261214089@qq.com>
2024-01-17 14:13:10 +0800

    update

---
+++ search-insert-position.c
@@ -0,0 +1,16 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    for(int i = 0; i < numsSize; i++)
+    {
+        if(target == nums[i])
+        {
+            return i;
+        }
+
+        if(target < nums[i])
+        {
+            return i;
+        }
+    }
+
+    return numsSize-1;
+}
