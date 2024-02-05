d9d31db84c078585158fc7cf196e24b3a6023f22
marsevilspirit <a2261214089@qq.com>
2024-02-05 18:32:37 +0800

    powx-n

---
+++ powx-n.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    
+    double qpow(double a, long long b){
+        double res = 1;
+        while(b){
+            if(b&1) res = res*a;
+            b >>= 1;
+            a *= a;
+        }
+        return res;
+    }
+    
+    
+    double myPow(double x, long long n) {
+        if(n == 0) return 1;
+        if(n > 0) return qpow(x,n);
+        if(n < 0) return 1/qpow(x,-n);
+        return 1.0;
+    }
+};
